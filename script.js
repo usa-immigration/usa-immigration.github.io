79cdd51b29d1d6f6742de75269d0223224232b22
Glitch (right-surprise) <none>
2017-11-12 18:22:50 +0000

    Checkpoint

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
  
 $(function(){
+   
+  $(window).on('beforeunload', function(){
+    
+    $(window).scrollTop(0);
+  });
   
   
- // alert("hi");
+  $(window).scroll(function() {
+   if($(document).scrollTop() > 200){
+/*$("#header").animate({'background-color': '#A5243D'});
+     $("#site-name").animate({'color': "white"});
+          $(".nav-menu a").animate({'color': "white"});
+          */ 
+     
+$("#header").addClass("testings");
+      
+  $(".nav-menu").addClass("nav-menu1");
+ $("ul.sub-menu a").css("color", "black");
+   }else{
+     $("#header").removeClass("testings");
+     $(".nav-menu").removeClass("nav-menu1");
+   }
+    
+    
+  });
+  
+  
+  
   
  $("#header").load("header.html"); 
   $("footer").load("footer.html"); 
